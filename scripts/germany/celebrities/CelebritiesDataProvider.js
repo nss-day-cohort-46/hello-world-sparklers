d2474d8f7ee4f8b524c24c291e9a469cfdfabaee
Alex Martin <alex@subtleco.co>
2021-01-19 14:24:29 -0600

    save edits

--- scripts/germany/celebrities/CelebritiesDataProvider.js
+++ scripts/germany/celebrities/CelebritiesDataProvider.js
@@ -1,16 +1,16 @@
 const celebritiesCollection = [
     {
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9zuXMVWGLEWC5jSzdMW9FrqtmD6tfhjOhjw&usqp=CAU"
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9zuXMVWGLEWC5jSzdMW9FrqtmD6tfhjOhjw&usqp=CAU",
         name: "Dominic Monaghan"
     },
     {
-        image: "https://cdn0.scrvt.com/c2465e9022ba946df66d1244a69b1c75/fad688b1344788ce/3bd431f663ff/v/bf0c92b85df4/p_0300_11_A2_021_o2.jpg"
+        image: "https://cdn0.scrvt.com/c2465e9022ba946df66d1244a69b1c75/fad688b1344788ce/3bd431f663ff/v/bf0c92b85df4/p_0300_11_A2_021_o2.jpg",
         name: "Boris Becker"
     },
     {
-        image: "https://media.newyorker.com/photos/5eb5e5b63448f87c8f2b8e71/4:3/w_1841,h_1381,c_limit/200518_r36465.jpg"
+        image: "https://media.newyorker.com/photos/5eb5e5b63448f87c8f2b8e71/4:3/w_1841,h_1381,c_limit/200518_r36465.jpg",
         name: "Henry Kissinger"
-    },
+    }
 ]
 
 export const UseCelebrities = () => [...celebritiesCollection];
