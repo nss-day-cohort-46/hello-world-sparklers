3dda8fc61a4c0b34299e85e5a804250a62dbab4a
Dylan Morris <dylanrobertmorris@gmail.com>
2021-01-19 12:30:46 -0600

    added js for cities directory

---
+++ scripts/iceland/cities/CitiesList.js
@@ -0,0 +1,16 @@
+import { useCities } from './CitiesDataProvider.js/'
+import { City } from './Cities.js'
+
+export const CitiesList = () => {
+    const contentElement = document.querySelector(".countryContainer__cities")
+    const cities = useCities()
+    
+    let citiesHTMLRepresentation = ""
+    for (const city of cities) {
+        citiesHTMLRepresentation += City(city)
+    }
+        
+    contentElement.innerHTML += 
+        `${citiesHTMLRepresentation}`
+    
+}
