57f5e23c3bb15816325ed75bc0291c41db7dc67d
peytonkemp <peyton.kemp@gmail.com>
2021-01-19 14:21:56 -0600

    add end quotes to city card population

--- scripts/mexico/cities/Cities.js
+++ scripts/mexico/cities/Cities.js
@@ -2,8 +2,8 @@ export const Cities = (city) => {
     return `
     <article class="cityCard">
         <img src="${city.image}" alt"" class="cityCard__img">
-        <p class="cityCard__name">${city.name}
-        <p class="cityCard__population>${city.population}
+        <p class="cityCard__name">${city.name}</p>
+        <p class="cityCard__population">${city.population}</p>
     </article>`
 }
 
