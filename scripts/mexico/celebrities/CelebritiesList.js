d8c3e0fce97967932c193bbc6a22536f2b69c2b1
peytonkemp <peyton.kemp@gmail.com>
2021-01-19 15:08:40 -0600

    completed the 3 celebrities js's

---
+++ scripts/mexico/celebrities/CelebritiesList.js
@@ -0,0 +1,15 @@
+import { useCelebrities } from './CelebritiesDataProvider.js'
+import { Celebrities } from './Celebrities.js'
+
+export const CelebritiesList = () => {
+    const contentElement = document.querySelector(".countryContainer__citizens")
+    const celebrities = useCelebrities()
+    let citiesHTMLRepresentations = ""
+    for (const celeb of celebrities) {
+        celebritiesHTMLRepresentations += Celebrities(celeb)
+    }
+    contentElement.innerHTML += `
+        ${celebritiesHTMLRepresentations}
+    `
+}
+
--- scripts/mexico/main.js
+++ scripts/mexico/main.js
@@ -1,2 +1,5 @@
 import { CitiesList } from './cities/CitiesList.js'
 CitiesList()
+
+import { CelebritiesList } from './celebrities/CelbritiesList.js'
+CelebritiesList()
