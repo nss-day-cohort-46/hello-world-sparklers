026ad90b27c31026683bb9349f25987cb3efc772
Caleb James <calebsjames@gmail.com>
2021-01-19 13:08:25 -0600

    moved image element

--- scripts/monaco/cities/Cities.js
+++ scripts/monaco/cities/Cities.js
@@ -1,9 +1,9 @@
 export const Cities = (city) => {
     return `
         <article class="cityCard">
+            <img class="cityCard__img" src="${city.image}" />
             <div class="cityCard__name">${city.name}</div>
             <div class="cityCard__population">${city.population}</div>
-            <img class="cityCard__img" src="${city.image}" />
         </article>
     `
 }
