32127ad662f2d5b589d22b6b87352447f2eab799
Caleb James <calebsjames@gmail.com>
2021-01-20 10:20:28 -0600

    html changes

--- scripts/monaco/celebrities/Celebrities.js
+++ scripts/monaco/celebrities/Celebrities.js
@@ -1,7 +1,7 @@
 export const Celebrity = (celebrity) => {
     return `
         <article class="countryContainer__card">
-            <img class="countryContainer__card-img" src="${celebrity.image}"</div>
+            <img class="countryContainer__card-img" src="${celebrity.image}"/>
             <p class="countryContainer__card-name">
                 <a href="${celebrity.wiki}">${celebrity.name}</a>
             </p>
